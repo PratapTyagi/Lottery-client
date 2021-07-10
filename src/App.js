a506a00f211af857800f830c0469c46f3e0576cc
Pratap Tyagi <tyagipratap111@gmail.com>
2021-07-10 18:55:06 +0530

    Data rendered from the contract

--- src/App.js
+++ src/App.js
@@ -5,13 +5,37 @@ import lottery from "./lottery.js";
 
 function App() {
   const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
+
   useEffect(() => {
     const getManager = async () => await lottery.methods.manager().call();
     getManager().then((data) => setManager(data));
+    const getPlayers = async () => await lottery.methods.getPlayers().call();
+    getPlayers().then((data) => setPlayers(data));
+    const getBalance = async () =>
+      await web3.eth.getBalance(lottery.options.address);
+    getBalance().then((data) => setBalance(data));
   }, []);
+
   return (
-    <div className="App">
-      <h1>Manager: {manager}</h1>
+    <div className="app">
+      <header>
+        <h1>Lottery App</h1>
+      </header>
+      <div className="app__top">
+        <h4>
+          Manager:
+          <p>{manager}</p>
+        </h4>
+        <h4>
+          No. of players: <br /> {players.length}
+        </h4>
+        <h4>
+          Winning prize: <br /> {balance} ether
+        </h4>
+      </div>
     </div>
   );
 }
